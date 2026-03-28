cf47565457e336ebd56265e2b07a500fa819d65b
Yashashvi Singh <your-github-email@gmail.com>
2026-03-28 13:45:32 +0530

    Initial Spring Boot social media platform project

---
+++ socialmediaplatform/src/main/java/com/capg/SocialmediaplatformApplication.java
@@ -0,0 +1,13 @@
+package com.capg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediaplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediaplatformApplication.class, args);
+	}
+
+}
