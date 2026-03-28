4badea81379abbdf24fe6b822d017baab9a7db04
Mansi Roy <roymansi911@gmail.com>
2026-03-28 15:54:07 +0530

    Added Friends module (serviceImpl, service, dto)

---
+++ socialmediaplatform/src/main/java/com/capg/service/FriendsService.java
@@ -0,0 +1,19 @@
+package com.capg.service;
+
+import com.capg.dto.FriendsDTO;
+import java.util.List;
+
+public interface FriendsService {
+
+    FriendsDTO addFriend(FriendsDTO dto);
+
+    List<FriendsDTO> getAllFriends();
+
+    FriendsDTO getFriendById(Integer id);
+
+    void deleteFriend(Integer id);
+
+    List<FriendsDTO> getPendingRequests(Integer userId);
+
+    List<FriendsDTO> getAcceptedFriends(Integer userId);
+}
