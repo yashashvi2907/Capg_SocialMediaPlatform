060b8798116bceec63658c9b94a91d44e9929741
Mansi Roy <roymansi911@gmail.com>
2026-04-02 12:57:58 +0530

    Add exception class

---
+++ socialmediaplatform/src/main/java/com/capg/exception/UnauthorizedException.java
@@ -0,0 +1,9 @@
+package com.capg.exception;
+
+public class UnauthorizedException extends RuntimeException {
+
+	    public UnauthorizedException(String message) {
+	        super(message);
+	    }
+	}
+
