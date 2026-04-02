0e6009513dd2bab2be340828d2ebb8c1928d7124
Yashashvi Singh <your-github-email@gmail.com>
2026-04-02 20:57:32 +0530

    Modified Exception Module

--- socialmediaplatform/src/main/java/com/capg/exception/GlobalExceptionHandler.java
+++ socialmediaplatform/src/main/java/com/capg/exception/GlobalExceptionHandler.java
@@ -1,8 +1,6 @@
 package com.capg.exception;
 
 import com.capg.dto.ErrorDTO;
-import com.capg.exception.FriendNotFoundException;
-import com.capg.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
