ff6c49aabb60f1b9c12b45b1528184d11030cc8b
Mansi Roy <roymansi911@gmail.com>
2026-04-02 11:30:25 +0530

    modified UserAccount

--- socialmediaplatform/src/main/java/com/capg/controller/UserAccountController.java
+++ socialmediaplatform/src/main/java/com/capg/controller/UserAccountController.java
@@ -1,79 +1,75 @@
-//package com.capg.controller;
+package com.capg.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.capg.dto.LoginDTO;
+import com.capg.entity.UserAccount;
+import com.capg.security.JwtUtil;
+import com.capg.service.IUserAccountService;
+
+
+@RestController
+@RequestMapping("/account")
+public class UserAccountController {
+
+    @Autowired
+    private IUserAccountService service;
+    
+    @PostMapping("/login")
+    public String login(@RequestBody LoginDTO dto) {
+
+        UserAccount user = service.login(dto.getUsername(), dto.getPassword());
+
+        String token = JwtUtil.generateToken(user.getUsername());
+
+        return "Bearer " + token;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+//  Register API
+// @PostMapping("/register")
+// public UserAccount register(@RequestBody UserAccount account) {
+//     return service.register(account);
+// }
+
+// @PostMapping("/login")
+// public String login(@RequestBody LoginDTO dto, HttpSession session) {
 //
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
+//     UserAccount user = service.login(dto.getUsername(), dto.getPassword());
 //
-//import com.capg.dto.LoginDTO;
-//import com.capg.entity.UserAccount;
-////import com.capg.security.JwtUtil;
-////import com.capg.exception.UnauthorizedException;
-//import com.capg.service.IUserAccountService;
-////import jakarta.servlet.http.HttpSession;
+//     session.setAttribute("loggedUser", user);
 //
-//@RestController
-//@RequestMapping("/account")
-//public class UserAccountController {
+//     // Store login time
+//     session.setAttribute("loginTime", System.currentTimeMillis());
 //
-//    @Autowired
-//    private IUserAccountService service;
+//     // 1 minute session
+//     session.setMaxInactiveInterval(60);
 //
-//    @PostMapping("/login")
-//    public String login(@RequestBody LoginDTO dto) {
-//
-//        UserAccount user = service.login(dto.getUsername(), dto.getPassword());
-//
-//        if (user != null) {
-//            String token = JwtUtil.generateToken(user.getUsername());
-//            return token;
+//     return "Login Successful!!  Welcome " + user.getUsername();
+// }
+    
+    
+//    public void checkLogin(HttpSession session) {
+//        if (session.getAttribute("loggedUser") == null) {
+//        	throw new UnauthorizedException("Please login first");
 //        }
-//
-//        return "Invalid credentials";
 //    }
-//}
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//    
-////  Register API
-//// @PostMapping("/register")
-//// public UserAccount register(@RequestBody UserAccount account) {
-////     return service.register(account);
-//// }
-//
-//// @PostMapping("/login")
-//// public String login(@RequestBody LoginDTO dto, HttpSession session) {
-////
-////     UserAccount user = service.login(dto.getUsername(), dto.getPassword());
-////
-////     session.setAttribute("loggedUser", user);
-////
-////     // Store login time
-////     session.setAttribute("loginTime", System.currentTimeMillis());
-////
-////     // 1 minute session
-////     session.setMaxInactiveInterval(60);
-////
-////     return "Login Successful!!  Welcome " + user.getUsername();
-//// }
-//    
-//    
-////    public void checkLogin(HttpSession session) {
-////        if (session.getAttribute("loggedUser") == null) {
-////        	throw new UnauthorizedException("Please login first");
-////        }
-////    }
--- socialmediaplatform/src/main/java/com/capg/service/IUserAccountService.java
+++ socialmediaplatform/src/main/java/com/capg/service/IUserAccountService.java
@@ -5,6 +5,5 @@ import com.capg.entity.UserAccount;
 public interface IUserAccountService {
 
     UserAccount register(UserAccount account);
-
     UserAccount login(String username, String password);
 }
