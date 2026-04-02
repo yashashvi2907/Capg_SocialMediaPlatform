8372b8a55365caf78995fc0b0bce90569fb70c22
Mansi Roy <roymansi911@gmail.com>
2026-04-02 10:54:54 +0530

    Added userAccount Modules

---
+++ socialmediaplatform/src/main/java/com/capg/controller/UserAccountController.java
@@ -0,0 +1,79 @@
+//package com.capg.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import com.capg.dto.LoginDTO;
+//import com.capg.entity.UserAccount;
+////import com.capg.security.JwtUtil;
+////import com.capg.exception.UnauthorizedException;
+//import com.capg.service.IUserAccountService;
+////import jakarta.servlet.http.HttpSession;
+//
+//@RestController
+//@RequestMapping("/account")
+//public class UserAccountController {
+//
+//    @Autowired
+//    private IUserAccountService service;
+//
+//    @PostMapping("/login")
+//    public String login(@RequestBody LoginDTO dto) {
+//
+//        UserAccount user = service.login(dto.getUsername(), dto.getPassword());
+//
+//        if (user != null) {
+//            String token = JwtUtil.generateToken(user.getUsername());
+//            return token;
+//        }
+//
+//        return "Invalid credentials";
+//    }
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//    
+////  Register API
+//// @PostMapping("/register")
+//// public UserAccount register(@RequestBody UserAccount account) {
+////     return service.register(account);
+//// }
+//
+//// @PostMapping("/login")
+//// public String login(@RequestBody LoginDTO dto, HttpSession session) {
+////
+////     UserAccount user = service.login(dto.getUsername(), dto.getPassword());
+////
+////     session.setAttribute("loggedUser", user);
+////
+////     // Store login time
+////     session.setAttribute("loginTime", System.currentTimeMillis());
+////
+////     // 1 minute session
+////     session.setMaxInactiveInterval(60);
+////
+////     return "Login Successful!!  Welcome " + user.getUsername();
+//// }
+//    
+//    
+////    public void checkLogin(HttpSession session) {
+////        if (session.getAttribute("loggedUser") == null) {
+////        	throw new UnauthorizedException("Please login first");
+////        }
+////    }
