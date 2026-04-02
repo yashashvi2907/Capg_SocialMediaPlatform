16350e2dedd33f7229c9febe43bd6e44a716f0b1
Mansi Roy <roymansi911@gmail.com>
2026-04-02 15:23:35 +0530

    Modified globalException based on friends entity

--- socialmediaplatform/src/main/java/com/capg/exception/GlobalExceptionHandler.java
+++ socialmediaplatform/src/main/java/com/capg/exception/GlobalExceptionHandler.java
@@ -4,9 +4,13 @@ import com.capg.dto.ErrorDTO;
 import com.capg.exception.FriendNotFoundException;
 import com.capg.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -44,6 +48,30 @@ public class GlobalExceptionHandler {
                 LocalDate.now(),
                 "Message API Error"
         );
+    }
+    
+ // Friend Not Found
+    @ExceptionHandler(FriendNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleFriendNotFound1(FriendNotFoundException ex) {
+
+        Map<String, Object> error = new HashMap<>();
+        error.put("timestamp", LocalDateTime.now());
+        error.put("message", ex.getMessage());
+        error.put("status", HttpStatus.NOT_FOUND.value());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    // Generic Exception (optional but good)
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGeneric(Exception ex) {
+
+        Map<String, Object> error = new HashMap<>();
+        error.put("timestamp", LocalDateTime.now());
+        error.put("message", ex.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
 
