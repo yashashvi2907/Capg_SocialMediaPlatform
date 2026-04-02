e8630e9e4de97c19681f12bdb9a95d82ec7313c4
Yashashvi Singh <your-github-email@gmail.com>
2026-04-02 20:56:05 +0530

    Modified GlobalExceptionHandler

--- socialmediaplatform/src/main/java/com/capg/exception/GlobalExceptionHandler.java
+++ socialmediaplatform/src/main/java/com/capg/exception/GlobalExceptionHandler.java
@@ -15,30 +15,6 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    //Handle Friend Not Found Exception
-    @ExceptionHandler(FriendNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorDTO handleFriendNotFound(FriendNotFoundException ex) {
-
-        return new ErrorDTO(
-                ex.getMessage(),
-                LocalDate.now(),
-                "Friend API Error"
-        );
-    }
-
-    //Handle Generic Exception
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorDTO handleGeneralException(Exception ex) {
-
-        return new ErrorDTO(
-                ex.getMessage(),
-                LocalDate.now(),
-                "Internal Server Error"
-        );
-    }
-
     // Handle Message Not Found Exception
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
