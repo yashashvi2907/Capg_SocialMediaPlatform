6ded9823656e60fc2c18bc0205bda2c69f614e19
Yashashvi Singh <your-github-email@gmail.com>
2026-04-02 10:15:42 +0530

    Modified User Module and Added GetApi of User

--- socialmediaplatform/src/main/java/com/capg/entity/User.java
+++ socialmediaplatform/src/main/java/com/capg/entity/User.java
@@ -1,14 +1,19 @@
 package com.capg.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Table(name="users")
+@Getter
+@Setter
 public class User {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "userID")
 	private Integer userID;
 
@@ -51,116 +56,4 @@ public class User {
 
 	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
 	private UserAccount userAccount;
-
-	public Integer getUserID() {
-		return userID;
-	}
-
-	public void setUserID(Integer userID) {
-		this.userID = userID;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public byte[] getProfilePicture() {
-		return profilePicture;
-	}
-
-	public void setProfilePicture(byte[] profilePicture) {
-		this.profilePicture = profilePicture;
-	}
-
-	public List<Post> getPosts() {
-		return posts;
-	}
-
-	public void setPosts(List<Post> posts) {
-		this.posts = posts;
-	}
-
-	public List<Comment> getComments() {
-		return comments;
-	}
-
-	public void setComments(List<Comment> comments) {
-		this.comments = comments;
-	}
-
-	public List<Likes> getLikes() {
-		return likes;
-	}
-
-	public void setLikes(List<Likes> likes) {
-		this.likes = likes;
-	}
-
-	public List<Notification> getNotifications() {
-		return notifications;
-	}
-
-	public void setNotifications(List<Notification> notifications) {
-		this.notifications = notifications;
-	}
-
-	public List<Message> getSentMessages() {
-		return sentMessages;
-	}
-
-	public void setSentMessages(List<Message> sentMessages) {
-		this.sentMessages = sentMessages;
-	}
-
-	public List<Message> getReceivedMessages() {
-		return receivedMessages;
-	}
-
-	public void setReceivedMessages(List<Message> receivedMessages) {
-		this.receivedMessages = receivedMessages;
-	}
-
-	public List<Friends> getFriends1() {
-		return friends1;
-	}
-
-	public void setFriends1(List<Friends> friends1) {
-		this.friends1 = friends1;
-	}
-
-	public List<Friends> getFriends2() {
-		return friends2;
-	}
-
-	public void setFriends2(List<Friends> friends2) {
-		this.friends2 = friends2;
-	}
-
-	public UserAccount getUserAccount() {
-		return userAccount;
-	}
-
-	public void setUserAccount(UserAccount userAccount) {
-		this.userAccount = userAccount;
-	}
 }
--- socialmediaplatform/src/main/java/com/capg/service/UserService.java
+++ socialmediaplatform/src/main/java/com/capg/service/UserService.java
@@ -1,9 +1,10 @@
 package com.capg.service;
+import com.capg.dto.PostDTO;
 
-//import com.capg.dto.PostDto;
 import java.util.List;
 
 public interface UserService {
 
-//    List<PostDto> getUserPosts(Integer userId);
+    List<PostDTO> getUserPosts(Integer userId);
+
 }
