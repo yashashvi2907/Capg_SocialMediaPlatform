9a09bfd67ae61a291a0563ce19a2d78cc6ca59b2
Mansi Roy <roymansi911@gmail.com>
2026-04-03 16:08:39 +0530

    adding documentation in UserAccount

--- socialmediaplatform/src/main/java/com/capg/entity/UserAccount.java
+++ socialmediaplatform/src/main/java/com/capg/entity/UserAccount.java
@@ -1,78 +1,163 @@
 package com.capg.entity;
 
 import jakarta.persistence.*;
-
 import java.time.LocalDateTime;
 
+/**
+ * UserAccount Entity
+ * 
+ * This class represents the account details of a user in the system.
+ * It is mainly used for authentication and login management.
+ * 
+ * Table Name: user_account
+ * 
+ * Description:
+ * - Stores login credentials and account status
+ * - Maintains last login time
+ * - Linked with User entity (One-to-One relationship)
+ * 
+ * Fields:
+ * - accountId  : Unique account ID (Primary Key)
+ * - username   : Username of the account
+ * - password   : Password (cannot be null)
+ * - loginDate  : Last login timestamp
+ * - status     : Account status (ACTIVE / INACTIVE)
+ * - user       : Associated User entity
+ */
 @Entity
 @Table(name = "user_account")
 public class UserAccount {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer accountId;
+    /**
+     * Primary Key - Unique Account ID
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer accountId;
 
-	private String username;
+    /**
+     * Username of the account
+     */
+    private String username;
 
-	@Column(nullable = false)
-	private String password;
+    /**
+     * Password of the account (mandatory field)
+     */
+    @Column(nullable = false)
+    private String password;
 
-	private LocalDateTime loginDate;
+    /**
+     * Last login date and time
+     */
+    private LocalDateTime loginDate;
 
-	@Column(nullable = false)
-	private String status;
+    /**
+     * Account status (ACTIVE / INACTIVE)
+     */
+    @Column(nullable = false)
+    private String status;
 
-	@OneToOne
-	@JoinColumn(name = "userID", nullable = false, unique = true)
-	private User user;
+    /**
+     * One-to-One relationship with User entity
+     * Each account is linked to one user
+     */
+    @OneToOne
+    @JoinColumn(name = "userID", nullable = false, unique = true)
+    private User user;
 
-	public Integer getAccountId() {
-		return accountId;
-	}
+    /**
+     * Get Account ID
+     * @return accountId
+     */
+    public Integer getAccountId() {
+        return accountId;
+    }
 
-	public void setAccountId(Integer accountId) {
-		this.accountId = accountId;
-	}
+    /**
+     * Set Account ID
+     * @param accountId unique ID
+     */
+    public void setAccountId(Integer accountId) {
+        this.accountId = accountId;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    /**
+     * Get Username
+     * @return username
+     */
+    public String getUsername() {
+        return username;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    /**
+     * Set Username
+     * @param username user's username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public LocalDateTime getLoginDate() {
-		return loginDate;
-	}
+    /**
+     * Get Password
+     * @return password
+     */
+    public String getPassword() {
+        return password;
+    }
 
-	public void setLoginDate(LocalDateTime loginDate) {
-		this.loginDate = loginDate;
-	}
+    /**
+     * Set Password
+     * @param password user's password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getStatus() {
-		return status;
-	}
+    /**
+     * Get Login Date
+     * @return loginDate
+     */
+    public LocalDateTime getLoginDate() {
+        return loginDate;
+    }
 
-	public void setStatus(String status) {
-		this.status = status;
-	}
+    /**
+     * Set Login Date
+     * @param loginDate last login time
+     */
+    public void setLoginDate(LocalDateTime loginDate) {
+        this.loginDate = loginDate;
+    }
 
-	public User getUser() {
-		return user;
-	}
+    /**
+     * Get Account Status
+     * @return status (ACTIVE / INACTIVE)
+     */
+    public String getStatus() {
+        return status;
+    }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
+    /**
+     * Set Account Status
+     * @param status account status
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    /**
+     * Get Associated User
+     * @return User entity
+     */
+    public User getUser() {
+        return user;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-
+    /**
+     * Set Associated User
+     * @param user linked user entity
+     */
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- socialmediaplatform/src/main/java/com/capg/service/IUserAccountService.java
+++ socialmediaplatform/src/main/java/com/capg/service/IUserAccountService.java
@@ -2,8 +2,34 @@ package com.capg.service;
 
 import com.capg.entity.UserAccount;
 
+/**
+ * IUserAccountService (Service Interface)
+ * 
+ * This interface defines all business operations related to
+ * User Account management in the Social Media Platform.
+ * 
+ * It acts as a bridge between Controller and Repository layers.
+ * 
+ * Responsibilities:
+ * - Register new user
+ * - Authenticate user (login)
+ */
 public interface IUserAccountService {
 
+    /**
+     * Register a new user account
+     * 
+     * @param account UserAccount object containing user details
+     * @return saved UserAccount object
+     */
     UserAccount register(UserAccount account);
+
+    /**
+     * Authenticate user login using username and password
+     * 
+     * @param username user's username
+     * @param password user's password
+     * @return UserAccount object if login successful
+     */
     UserAccount login(String username, String password);
 }
